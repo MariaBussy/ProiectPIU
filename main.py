83e9c914fe8aefffc4c29739173fdd717824d093
Iosif Vieru <iosifvieru440@gmail.com>
2024-11-27 15:40:50 +0200

    epub controller

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from database.models import Book
 from controllers.authors_controller import *
 from controllers.books_controller import *
 from controllers.bookmarks_controller import *
+from controllers.epub_controller import *
 
 if __name__ == "__main__":
-    print("Autori: ", get_authors())
-    print("Carti: ", get_books())
+    file = "blanchard-a-dear-little-girl.epub"
+    print(process_epub(file))
