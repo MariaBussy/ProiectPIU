4893c036e923e3d0404122f7731581c0ade962b2
maria-busaga <maria.busaga@student.tuiasi.ro>
2024-12-04 15:37:58 +0200

    updated with genre

--- recommandation/googleBooksScraper.py
+++ recommandation/googleBooksScraper.py
@@ -31,18 +31,37 @@ def get_book_info(book_name, author_name, api_key):
 api_key = "AIzaSyBx5Psf4RSTDW8Br_dWkc8618JkFIMkFYQ"  # Replace with your Google Books API key
 
 complete_info=[]
-for book in data:
-    book_data=get_book_info(book['title'],  book['author'], api_key)
+for sample_book in data:
+    book_data=get_book_info(sample_book['title'],  sample_book['author'], api_key)
+    print(book_data)
     book_description=''
     preview_link = ''
-    for i in range (len(book_data['items'])):
-        if book_data['items'][i]['volumeInfo']['description'] and book_description=='' and book_data['items'][i]['volumeInfo']['previewLink'] and preview_link=='':
-            book_description=book_data['items'][i]['volumeInfo']['description']
-            preview_link=book_data['items'][i]['volumeInfo']['previewLink']
+    book_genre = []
+    for book in book_data['items'][2:]:
+        volume_info = book.get('volumeInfo', {})
+        description = volume_info.get('description')
+        preview_link_check = volume_info.get('previewLink')
+        categories = volume_info.get('categories')
+        language = volume_info.get('language')
+
+        print(
+            f"Description: {description}, Preview Link: {preview_link_check}, Categories: {categories}, Language: {language}")
+
+        if (
+                description and
+                preview_link_check and
+                categories and
+                language == 'en'
+        ):
+            book_description = description
+            preview_link = preview_link_check
+            book_genre = categories
             break
+
     complete_info.append({
-        'title': book_data['items'][0]['volumeInfo']['title'],
-        'author': book_data['items'][0]['volumeInfo']['authors'],
+        'title': sample_book['title'],
+        'author': sample_book['author'],
+        'genre': book_genre,
         'description': book_description,
         'preview_link': preview_link
     })
